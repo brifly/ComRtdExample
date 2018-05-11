b5f6da40a482bf63ece5316a1a3e3892a639bdfc
Brian Flynn <bflynn@eclipseoptions.com>
2018-05-11 14:25:03 +0800

    Add Sample Files

---
+++ IRTDUpdateEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ComRtdExample
+{
+    [Guid("A43788C1-D91B-11D3-8F39-00C04F3651B8")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    [ComImport]
+    public interface IRTDUpdateEvent
+    {
+        [DispId(10)]
+        void UpdateNotify();
+
+        [DispId(11)]
+        int HeartbeatInterval { get; set; }
+
+        [DispId(12)]
+        void Disconnect();
+    }
+}
---
+++ RtdServer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ComRtdExample
+{
+    [ComVisible(true)]
+    [ProgId("ComSample.RtdServer")]
+    [Guid("F465CAAC-70CB-494C-A3A4-2C95835A974C")]
+    public class RtdServer : IRtdServer
+    {
+        public int ServerStart(IRTDUpdateEvent CallbackObject)
+        {
+            return 1;
+        }
+
+        public object ConnectData(int topicId, ref Array strings, ref bool newValues)
+        {
+            return 2;
+        }
+
+        public Array RefreshData(ref int topicCount)
+        {
+            return new[] { 2 };
+        }
+
+        public void DisconnectData(int topicID)
+        {
+
+        }
+
+        public int Heartbeat()
+        {
+            return 1;
+        }
+
+        public void ServerTerminate()
+        {
+        }
+
+        public string GetValue()
+        {
+            return nameof(RtdServer);
+        }
+    }
+}
